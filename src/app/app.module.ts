483a6dc99afb586f428af9b97fa9c4316e32e8bf
Renato Pradebon <renatopradebon@hotmail.com>
2018-07-24 12:52:19 -0400

    COnteúdo do curso da Loiane - aula 4
    COnteúdo do curso da Loiane - aula 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+import {AppComponent} from './app.component';
+import {MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
+import {MeuPrimeiro2Component} from './meu-primeiro2/meu-primeiro2.component';
+import {CursosModule} from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {literal} from '@angular/compiler/src/output/output_ast';
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <p>My first component with Angular2</p>
+  `
+})
+export class MeuPrimeiroComponent {
+}
