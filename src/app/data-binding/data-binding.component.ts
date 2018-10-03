7f49ad3ce46621f47992360f3fa0eecc9e104d1f
Renato Pradebon <renato.pradebon@datapar.com.py>
2018-10-03 13:08:14 -0400

    ANGULAR2 PROFESSOR LOIANE  13 MINUTOS #11: Event binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -3,12 +3,21 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styleUrls: ['./data-binding.component.css'],
+  styles: ['.highlight {\n' +
+  '    background-color: yellow;\n' +
+  '    font-weight: bold;\n' +
+  '}']
 })
 export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.training';
   cursoAngular: boolean = true;
   urlImagem: string = 'https://picsum.photos/400/200/';
+
+  actualValue: string = '';
+  savedValue: string;
+
+  isMouseOver: boolean = false;
 
   getValor() {
     return 1;
@@ -24,4 +33,21 @@ export class DataBindingComponent implements OnInit {
   ngOnInit() {
   }
 
+  botaoClicado() {
+    alert('The button has clicked!');
+  }
+
+
+  onKeyUp(event: KeyboardEvent) {
+    console.log();
+    this.actualValue = (<HTMLInputElement>event.target).value;
+  }
+
+  saveValue(value) {
+    this.savedValue = value;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
 }
